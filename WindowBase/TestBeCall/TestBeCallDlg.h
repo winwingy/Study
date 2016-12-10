223ea160251080cf6bf297b7e8c39a75ef73d010
温杰 <winwingy@163.com>
2016-12-10 17:06:26 +0800

    windowbase

---
+++ WindowBase/TestBeCall/TestBeCallDlg.h
@@ -0,0 +1,35 @@
+
+// TestBeCallDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CTestBeCallDlg 对话框
+class CTestBeCallDlg : public CDialogEx
+{
+// 构造
+public:
+	CTestBeCallDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_TESTBECALL_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+    afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
+    afx_msg void OnBnClickedButton1();
+};
